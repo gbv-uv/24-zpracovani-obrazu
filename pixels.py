79f35f30912a6d535ef9c77bd80dc05b0d6b6031
Vaclav Uher <uv@gbv.cz>
2025-03-14 07:37:07 +0100

    add pixel manipulation example

---
+++ pixels.py
@@ -0,0 +1,22 @@
+from matplotlib.image import imread
+import matplotlib.pyplot as plt
+import numpy as np
+
+black = 0
+white = 255
+
+height = 600
+width = 800
+
+image = np.zeros(shape=(height, width), dtype=np.uint8).copy()
+
+for y in range(height):
+    for x in range(width):
+        if x % 2 == 0 and y % 2 == 0:
+            image[y, x] = white
+
+
+
+plt.figure(figsize= (15, 5))
+plt.imshow(image, cmap=plt.cm.gray, vmin=0, vmax=255)
+plt.show()
